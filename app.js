bcecd7dba41e94753cea116879384502c6040b69
Jason <92811609+Mister-Zeng@users.noreply.github.com>
2022-03-22 12:39:25 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Image')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+});
+
+app.listen(3000, () => {
+    console.log('listeing')
+});
