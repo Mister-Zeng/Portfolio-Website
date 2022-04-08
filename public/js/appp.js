73091e3314f8505649c537d737065df526687ae9
Jason <92811609+Mister-Zeng@users.noreply.github.com>
2022-04-08 11:59:30 -0400

    Update appp.js

--- public/js/appp.js
+++ public/js/appp.js
@@ -10,3 +10,21 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
