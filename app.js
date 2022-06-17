8c0da8bbeaabc2ce722d0745cca9d703e3921ee6
Jason <officialjasonzeng@gmail.com>
2022-06-17 11:08:41 -0400

    email

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }))
 
 function reveal() {
@@ -28,3 +28,45 @@ function reveal() {
 }
 
 window.addEventListener("scroll", reveal);
+
+
+const formsubmit = () => {
+
+  let email = document.getElementById("email-address");
+  let fullName = document.getElementById("full-name");
+  let msg = document.getElementById("message");
+  let submit = document.getElementById("formSubmit")
+
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (fullName.value == "" || email.value == "" || msg.value == "") {
+      emptyerror();
+    } else {
+      sendmail(fullName.value, email.value, msg.value,);
+      success();
+    }
+  })
+}
+formsubmit();
+
+function sendmail(fullName, email, msg) {
+  const formData = {
+    from_name: fullName,
+    to_name: email,
+    message: msg
+  };
+
+  emailjs.send('service_5un9k9c', 'template_73yyald', formData)
+}
+
+function emptyerror() {
+  swal("Oops...", "Fields cannot be empty!", "error");
+}
+
+function error() {
+  swal("Oops...", "Something went wrong!", "error");
+}
+
+function success() {
+  swal("Success...", "Successfully sent message", "success")
+}
